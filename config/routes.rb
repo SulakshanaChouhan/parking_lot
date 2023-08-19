ae8a1fe08d3768d6fb418c959a74d63ea4a0fd9a
SulakshanaChouhan <sulakshanachouhan@gmail.com>
2023-08-19 22:49:08 +0530

    Add Models and Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  # get 'parkings/index'
+  # get 'parkings/registration_numbers_by_color'
+  # get 'parkings/ticket_number_by_registration_number'
+  # get 'parkings/ticket_numbers_by_color'
+  # get 'parkings/park_car'
+  # get 'parkings/remove_car'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :parking_lots do
+    member do
+      get :registration_numbers_by_color
+      get :ticket_number_by_registration_number
+      get :ticket_numbers_by_color
+      post :park_car
+      patch :remove_car
+    end
+  end  
 end
